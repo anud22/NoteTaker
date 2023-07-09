ad91def11d981c65f13db7a55cf85641a3cf06fa
anuD <adilbagi22@gmail.com>
2023-07-09 14:38:29 -0700

    Updated port to deploy on Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const api = require('./routes/index.js');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
