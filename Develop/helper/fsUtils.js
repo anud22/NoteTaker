d3f42b01298a59d65e64b791973f425190e845e6
anuD <adilbagi22@gmail.com>
2023-07-08 21:47:46 -0700

    Handled post/delete request

--- Develop/helper/fsUtils.js
+++ Develop/helper/fsUtils.js
@@ -3,4 +3,28 @@ const util = require('util');
 
 var readFromFile = util.promisify(fs.readFile);
 
-module.exports = {readFromFile};
+var readAndAppend = (file, content) => {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        let parsedData = [];
+        if (data.length > 0) {
+            parsedData = JSON.parse(data);
+        }
+
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+    })
+}
+var writeToFile = (file, content) => {
+    fs.writeFile(file, JSON.stringify(content), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+module.exports = { readFromFile, readAndAppend, writeToFile };
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,19 +1,59 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const {readFromFile} = require('../helper/fsUtils.js');
-
-
+const { readFromFile, readAndAppend, writeToFile } = require('../helper/fsUtils.js');
+const uuid = require('../helper/uuid.js');
+const dbPath = path.resolve(path.join(__dirname, '../db/db.json'));
 const notes = express.Router();
-const dbPath = path.join(__dirname, '../db/db.json');
+
 notes.get('/', (req, res) => {
     readFromFile(dbPath).then((data) => {
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
+        let jsonData = [];
+        if (data.length > 0) {
+            jsonData = JSON.parse(data);
+        }
+       res.json(jsonData);
     }).catch((error) => {
         console.error('Error reading the db.json file:', error);
-        res.sendStatus(500);
+        res.status(400).json(error);
     });
 });
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    const newNotes = { notes_id: uuid(), title: title, text: text };
+    if (title && text) {
+        readAndAppend(dbPath, newNotes);
+        res.status(200).json(newNotes);
+        return;
+    } else {
+        res.status(400).json("Post request should include single title & text");
+    }
+})
+
+notes.delete('/:id', (req, res) => {
+    if (req.params.id) {
+        readFromFile(dbPath).then((data) => {
+            if (data.length == 0) {
+                res.status(400).json("No data found");
+                return;
+            }
+            const contents = JSON.parse(data);
+            console.log('old',contents);
+            const newContents = contents.filter(x => x.notes_id !== req.params.id);
+            console.log('new', newContents);
+            if (contents.length === newContents.length) {
+                res.status(400).json("id not found");
+                return;
+            }
+            writeToFile(dbPath, newContents);
+            res.status(200).json(`Deleted id ${req.params.id}`);
+        }).catch((error) => {
+            console.error('Error reading the db.json file:', error);
+            res.status(400).json(error);
+        });
+    } else {
+        res.status(400).json("Delete request should include id");
+    }
+})
 
 module.exports = notes;
